01888926de4faff3f09770d58c3b7e58043fb02f
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-03-18 18:20:45 +0100

    table printing in index

--- resources/views/pages/comicsView/index.blade.php
+++ resources/views/pages/comicsView/index.blade.php
@@ -5,6 +5,41 @@
 @section('main')
     <main>
         <h2>index di comics</h2>
-        {{dd ($comics)}}
+
+        <div
+            class="table-responsive"
+        >
+            <table
+                class="table table-primary"
+            >
+                <thead>
+                    <tr>
+                        {{-- <th scope="col">id</th> --}}
+                        <th scope="col">title</th>
+                        <th scope="col">description</th>
+                        <th scope="col">thumb</th>
+                        <th scope="col">price</th>
+                        <th scope="col">series</th>
+                        <th scope="col">sale date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comics as $item)
+                        
+                    <tr class="">
+                        {{-- <td>{{$item->id}}</td> --}}
+                        <td>{{$item['title']}}</td>
+                        <td>{{$item['description']}}</td>
+                        <td>{{$item['thumb']}}</td>
+                        <td>{{$item['price']}}</td>
+                        <td>{{$item['series']}}</td>
+                        <td>{{$item ['sale_date']}}</td>
+                    </tr>
+                    @endforeach
+                    
+                </tbody>
+            </table>
+        </div>
+        
     </main>
 @endsection
